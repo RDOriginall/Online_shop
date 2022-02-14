583108caab249f4e9407e4f8d419ffd0c5db03dc
rd-originall <r.d.originall15@gmail.com>
2022-02-14 23:13:11 +0330

    Dual language added | settings.py

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +108,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
+
+LANGUAGES = [
+    ('fa', _('Persian')),
+    ('en', _('English'))
+]
 
 TIME_ZONE = 'UTC'
 
