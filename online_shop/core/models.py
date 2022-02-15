ca8a6aedac442e0345062a0195bd9202e3010f70
rd-originall <r.d.originall15@gmail.com>
2022-02-15 19:33:33 +0330

    Created | BaseManager class

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 
 
+class BaseManager(models.Manager):
+
+    def full_archive(self):
+        return super().get_queryset()
+
+    def get_queryset(self):
+        return super().get_queryset().exclude(is_delete=True)
+
