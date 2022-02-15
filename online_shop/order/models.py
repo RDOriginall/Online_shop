019472e283e472ffe3d67305cdeed9394e5b1e54
rd-originall <r.d.originall15@gmail.com>
2022-02-15 22:57:54 +0330

    Created | OffCode model

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
+from core import BaseModel
+from product import Discount
 # Create your models here.
+
+class OffCode(Discount):
+    """
+    Off code is a code that makes a total discount on Order model
+    """
+    code = models.CharField(max_length=10, unique=True, verbose_name=_("Discount code"),
+    help_text=_("Please Enter Off code"))
+    users = models.ManyToManyField(to=Customer, related_name="codes", default=None, null=True,
+    verbose_name=_("User Off code"), help_text=_("which users have used this code?"))
+    
+
+
+class Order(BaseModel):
+    pass
+
+
+
+class OrderItem(BaseModel):
+    pass
