3165c8a248a362b2662abdab4b1c1993ebc5efc7
rd-originall <r.d.originall15@gmail.com>
2022-02-15 19:38:36 +0330

    created | BaseModel class

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -11,3 +11,19 @@ class BaseManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().exclude(is_delete=True)
 
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    objects = BaseManager()
+
+    is_delete = models.BooleanField(default=False, editable=False, db_index=True)
+
+    def delete(self, using=None, keep_parent=False):
+        self.is_delete = True
+        self.save(using=using)
+
+    def restore(self):
+        self.is_delete = False
+        self.save()
