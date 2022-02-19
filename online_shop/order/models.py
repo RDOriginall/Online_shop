de5ae8989a5487ad58678c5f98108ee64f44d1c9
rd-originall <r.d.originall15@gmail.com>
2022-02-19 21:44:13 +0330

    Fields of classes | completed models

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from core.models import BaseModel
 from product.models import Discount, Product, OffCode
 from django.core.validators import MinValueValidator
-from customer.models import Customer
+from customer.models import Customer, Address
 # Create your models here.
 
 
@@ -13,9 +13,14 @@ class Order(BaseModel):
     """
     total_price = models.FloatField(verbose_name=_("Total price"), validators=[MinValueValidator(0)])
     final_price = models.FloatField(verbose_name=_("Final proce"), validators=[MinValueValidator(0)])
-    customer = models.ForeignKey(to=Customer, verbose_name=_("customer cart"), on_delete=models.CASCADE)
+    off_code = models.ForeignKey(to=OffCode, on_delete=models.SET_NULL, null=True, blank=True)
+    customer = models.ForeignKey(to=Customer, verbose_name=_("customer cart"), on_delete=models.RESTRICT)
+    address = models.ForeignKey(to=Address, on_delete=models.RESTRICT, verbose_name=_("Address"), help_text=_("Please select your address"))
     status = models.CharField(max_length=1, default='U', verbose_name=_("Order Status"),
     choices=[('P', _('Paid')), ('U', _('Unpaid')), ('C', _('Canceled'))])
+
+    class Meta:
+        unique_together = [['off_code', 'customer']] # Just once Time Use from OffCode
 
 
 
@@ -23,9 +28,9 @@ class OrderItem(BaseModel):
     """
     OrderItem model contains Items that the client wants to order
     """
-    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name="items", 
+    order = models.ForeignKey(to=Order, on_delete=models.RESTRICT, related_name="items", 
     verbose_name=_("Receipt"), help_text=_("Please select your order"))
-    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name=_("Product name"),
+    product = models.ForeignKey(to=Product, on_delete=models.RESTRICT, verbose_name=_("Product name"),
     help_text=_("Please select Product Item to add"))
     count = models.PositiveIntegerField(default=1, verbose_name=_("number of Order Items"), 
     validators=[MinValueValidator(1)], help_text=_("How many Items would you like to order?"))
