9fb43357e63b91eec50a8a599cd5dffea32f5709
rd-originall <r.d.originall15@gmail.com>
2022-02-19 21:42:39 +0330

    MyUserManager & User | Added models

--- online_shop/core/models.py
+++ online_shop/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, UserManager
 # Create your models here.
 
 
@@ -32,3 +32,17 @@ class BaseModel(models.Model):
     def restore(self):
         self.is_delete = False
         self.save()
+
+
+class MyUserManager(UserManager):
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        username = extra_fields['phone']
+        return super().create_superuser(username, email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    objects = MyUserManager()
+
+    phone = models.CharField(max_length=13, unique=True)
+    USERNAME_FIELD = 'phone'
+
