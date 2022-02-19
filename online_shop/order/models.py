2d21ce7148404b46d9fd3f7b08102f6c677bc2cf
rd-originall <r.d.originall15@gmail.com>
2022-02-19 21:24:42 +0330

    deleted | OffCode

--- online_shop/order/models.py
+++ online_shop/order/models.py
@@ -1,20 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from core.models import BaseModel
-from product.models import Discount, Product
+from product.models import Discount, Product, OffCode
 from django.core.validators import MinValueValidator
 from customer.models import Customer
 # Create your models here.
-
-class OffCode(Discount):
-    """
-    Off code is a code that makes a total discount on Order model
-    """
-    code = models.CharField(max_length=10, unique=True, verbose_name=_("Discount code"),
-    help_text=_("Please Enter Off code"))
-    users = models.ManyToManyField(to=Customer, related_name="codes", default=None, null=True,
-    verbose_name=_("User Off code"), help_text=_("which users have used this code?"))
-    
 
 
 class Order(BaseModel):
