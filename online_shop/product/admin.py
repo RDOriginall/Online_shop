39f7fd3ef5b3c5557b83e0fc3bc7ab552595e68a
rd-originall <r.d.originall15@gmail.com>
2022-02-21 18:39:31 +0330

    registered | OffCode

--- online_shop/product/admin.py
+++ online_shop/product/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from product.models import Category, Discount, Product
+from product.models import Category, Discount, Product, OffCode
 # Register your models here.
 
 
 admin.site.register(Category)
 admin.site.register(Discount)
 admin.site.register(Product)
+admin.site.register(OffCode)
