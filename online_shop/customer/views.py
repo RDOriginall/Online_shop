53ab1a53d023b5c0963e7ab984da675b6c6af174
rd-originall <r.d.originall15@gmail.com>
2022-02-21 17:30:46 +0330

    deleted | apis from here

--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from customer.forms import ContactForm
 
-# Create your views here.
+from django.contrib import messages
+from django.http import HttpResponse
+from django.views.generic import CreateView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+"""
+def create_message(request):
+    messages.set_level(request, 0)
+    messages.add_message(request, messages.ERROR, "Invalid input!")
+    messages.info(request, "Please fill up the fields")
+    messages.success(request, "Your comment added successfully")
+    messages.warning(request, "This filed can't be empty")
+    return HttpResponse("New messages created!")
+"""
+
+
+def contact(request):
+    if request.method == 'POST':
+        messages.set_level(request, 0)
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Your comment added successfully!")
+            return HttpResponse("New comment sent successfully")
+        else:
+            messages.warning(request, "Field is empty or contains invalid input!")
+            return HttpResponse("Please try again!")
