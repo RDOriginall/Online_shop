121c90d41e99dfc0cdcdf7303eefa4ce1da22501
rd-originall <r.d.originall15@gmail.com>
2022-02-21 18:40:34 +0330

    added | urls

---
+++ online_shop/product/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from product.apis import *
+from product.views import *
+
+
+urlpatterns = [
+    path('product_list_api/', product_list_api, name='product_list_api'),
+    path('product_api/', ProductListApi.as_view(), name='product_list_api'),
+    path('product_api/<int:pk>', ProductDetailApi.as_view(), name='product_api'),
+    path('products', ProductListView.as_view(), name='products'),
+]
