24efa1b5f16f3ec538b90eca886b0f59fc3fad6a
rd-originall <r.d.originall15@gmail.com>
2022-02-21 18:40:55 +0330

    added | productView | View

--- online_shop/product/views.py
+++ online_shop/product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.http import Http404, JsonResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.views import View
 
-# Create your views here.
+from product.models import Product, Category
+from product.serializers import ProductSerializer
+
+from django.views import generic
+
+
+class ProductListView(generic.ListView):
+    model = Product
+    fields = '__all__'
+    template_name = 'product/product_list.html'
