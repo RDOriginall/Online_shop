fe9b769c6be86b85d3b4e8506f26e4ddc279c3fb
rd-originall <r.d.originall15@gmail.com>
2022-02-21 18:38:46 +0330

    hotfix

--- online_shop/order/admin.py
+++ online_shop/order/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from order.models import OffCode, Order, OrderItem
+from order.models import Order, OrderItem
 # Register your models here.
 
 
-admin.site.register(OffCode)
 admin.site.register(Order)
 admin.site.register(OrderItem)
