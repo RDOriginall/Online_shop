d3430edeeffc388f939677cfca5ee7a89c908893
rd-originall <r.d.originall15@gmail.com>
2022-02-22 17:19:21 +0330

    added | password | reset | change

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from customer.views import contact, CustomerLoginView, register, home
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordResetView
 
 urlpatterns = [
     path('comment/', contact, name='contact'),
     path('login/', CustomerLoginView.as_view(), name='login'),
     path('register/', register, name="register"),
-    path('home/', home, name="home")
+    path('home/', home, name="home"),
+    path('password_reset/', PasswordResetView.as_view(), name="password_reset"),
+    path('change_password/', PasswordChangeView.as_view(), name="change_password"),
+    path('logout/', LogoutView.as_view(), name="logout"),
+
 ]
