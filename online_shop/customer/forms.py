ba274776e17ee80fd1e95858e80cd106ed2d6ea8
rd-originall <r.d.originall15@gmail.com>
2022-02-22 16:27:26 +0330

    added | register for

--- online_shop/customer/forms.py
+++ online_shop/customer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm
+from core.models import User
 
 class ContactForm(forms.Form):
     name: forms.CharField( max_length=30, required=True)
@@ -8,3 +9,10 @@ class ContactForm(forms.Form):
 
     def __str__(self):
         return f"user:{self.name} says:\n{self.comment}"
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "phone", "password1", "password2"]
