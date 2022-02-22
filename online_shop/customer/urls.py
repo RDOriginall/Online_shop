ead3f6e2830f6b675f9f83b070a09e03071e69b4
rd-originall <r.d.originall15@gmail.com>
2022-02-22 16:26:19 +0330

    added url | login | register

--- online_shop/customer/urls.py
+++ online_shop/customer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from customer.views import contact
+from customer.views import contact, CustomerLoginView, register
 
 urlpatterns = [
-    path('comment/', contact, name='contact')
+    path('comment/', contact, name='contact'),
+    path('login/', CustomerLoginView.as_view(), name='login'),
+    path('register/', register, name="register")
 ]
