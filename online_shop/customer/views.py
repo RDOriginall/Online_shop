5fd6c55dcc9150453efde9d2eb159ee6d8bf79bc
rd-originall <r.d.originall15@gmail.com>
2022-02-22 16:27:12 +0330

    added | registerview

--- online_shop/customer/views.py
+++ online_shop/customer/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from customer.forms import ContactForm
-
+from customer.models import Customer
 from django.contrib import messages
 from django.http import HttpResponse
 from django.views.generic import CreateView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth.views import LoginView
+from customer.forms import RegisterForm
 
 """
 def create_message(request):
@@ -28,3 +30,21 @@ def contact(request):
         else:
             messages.warning(request, "Field is empty or contains invalid input!")
             return HttpResponse("Please try again!")
+
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+        
+        return redirect("customer/lognview_form.html")
+    else:
+        form = RegisterForm()
+
+    return render(response, "customer/register.html", {"form":form})
+
+
+class CustomerLoginView(LoginView):
+    template_name = 'customer/loginview_form.html'
+
