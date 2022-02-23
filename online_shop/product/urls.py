e8da87950683a4363e4ea0e22fd7a9686c93fa74
rd-originall <r.d.originall15@gmail.com>
2022-02-23 17:42:20 +0330

    added | url

--- online_shop/product/urls.py
+++ online_shop/product/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('product_list_api/', product_list_api, name='product_list_api'),
     path('product_api/', ProductListApi.as_view(), name='product_list_api'),
     path('product_api/<int:pk>', ProductDetailApi.as_view(), name='product_api'),
+    path('category_api/', CategoryListApi.as_view(), name='category_list_api'),
+    path('category_api/<int:pk>', CategoryListApi.as_view(), name='category_list_api'),
     path('products', ProductListView.as_view(), name='products'),
     path('category', CategoryListView.as_view(), name='category')
 ]
