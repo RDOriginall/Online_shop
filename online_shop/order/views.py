ed1e16ce89ac34ce2cd8b55f4a1f4b97114c590a
rd-originall <r.d.originall15@gmail.com>
2022-02-27 18:13:50 +0330

    added | viewset

--- online_shop/order/views.py
+++ online_shop/order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
 # Create your views here.
+from order.serializer import OrderItemSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
