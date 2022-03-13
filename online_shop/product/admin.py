b208d0a2f2bd4032a140ace317e08ef2f8e65f6e
rd-originall <r.d.originall15@gmail.com>
2022-03-13 13:27:44 +0330

    added  | categoryAdmin

--- online_shop/product/admin.py
+++ online_shop/product/admin.py
@@ -3,7 +3,13 @@ from product.models import Category, Discount, Product, OffCode
 # Register your models here.
 
 
-admin.site.register(Category)
 admin.site.register(Discount)
-admin.site.register(Product)
 admin.site.register(OffCode)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("__all__")
+    list_filter = ("root")
+    raw_id_fields = ("name_en")
+    ordering = ("name_en")
+
